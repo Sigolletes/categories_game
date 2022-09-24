847fe43593597119d7a295d8474fec95f9ac4a53
Sigolletes <juditreverter87@gmail.com>
2022-09-24 12:35:27 +0200

    Add logic

--- script.js
+++ script.js
@@ -16,10 +16,7 @@ const game = document.querySelector("#game");
 
 let player1 = document.querySelector("#name1");
 let player2 = document.querySelector("#name2");
-
-    // LOGICAL PURPOSES
-
-
+let list = document.querySelector("#list");
 
 
 
@@ -33,3 +30,19 @@ newGame.addEventListener("click", () => {
 
 });
 
+
+
+// CREATE A LIST OF CATEGORIES
+
+const categories = [
+    ["Clothes", "Vehicles", "Jobs", "Things of the Universe", "Movie characters"],
+    ["Animals", "Movies", "TV series", "Something yellow", "Videogames"],
+    ["Furniture", "Books", "Natural spaces", "Verbs", "Buildings"],
+    ["Relationships", "Sports", "Something red", "In a big company", "Ecology"],
+    ["Emotions", "Weapons", "Healthy lifestyle", "Cities", "Sport equipment"],
+    ["Tools", "Climate", "Adjectives", "Insects", "Strengths"],
+    ["In the kitchen", "Recreational activities", "Agriculture", "Technology", "Toodler things"],
+    ["Plants", "Elder things", "Food", "In the city", "Countries"],
+    ["In the countryside", "Science", "Romantic things", "Trees", "Music"],
+    ["Art", "Flowers", "Subjects", "In a stationer's shop", "Geniuses (science, art, music...)"]
+]
