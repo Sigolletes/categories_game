e9281fe1c01d4b91ae363fd3f6a7e85fcd386ab6
Sigolletes <juditreverter87@gmail.com>
2022-10-06 15:46:43 +0200

    Change the display of all pages

--- script.js
+++ script.js
@@ -1,33 +1,66 @@
-// 2 players, they can choose the name
-// Several lists with categories
-// A table with the results
-// New game button
-// Score marker
-
-
 
 // VARIABLES
 
-    // DOM SELECTORS
+    // PAGE 1
 
-const newGame = document.querySelector("#newGame");
-const start = document.querySelector("#start");
-const game = document.querySelector("#game");
+const 
+    page1 = document.querySelector("#page1"),
+    startGame = document.querySelector("#startGame"),
+    help = document.querySelector("#help"),
+    credits = document.querySelector("#credits");
 
-let player1 = document.querySelector("#name1");
-let player2 = document.querySelector("#name2");
-let list = document.querySelector("#list");
+    // PAGE 2
+
+const 
+    page2 = document.querySelector("#page2"),
+    start = document.querySelector("#start");
+
+let 
+    player1 = document.querySelector("#player1"),
+    player2 = document.querySelector("#player2");
+
+    // PAGE 3
+
+const 
+    page3 = document.querySelector("#page3"),
+    name1 = document.querySelector("#name1"),
+    name2 = document.querySelector("#name2"),
+    score1 = document.querySelector("#score1"),
+    score2 = document.querySelector("#score2");
+
+let
+    letter = document.querySelector("#letter"),
+    list = document.querySelector("#list");
+
+    // PAGE 4
+
+const 
+    page4 = document.querySelector("#page4"),
+    setScore = document.querySelector("#setScore");
+
+let
+    result1 = document.querySelector("#result1"),
+    result2 = document.querySelector("#result2");
 
 
 
 // START NEW GAME
 
-newGame.addEventListener("click", () => {
-    if (player1 !== "" && player2 !== ""){
-        start.style.display = "none";
+newGame.addEventListener("click", (event) => {
+    if (player1.value !== "" && player2.value !== ""){
+    /*     names.style.display = "none";
+        finish.style.display = "none"; */
         game.style.display = "flex";
-    }
+        name1.innerText = `${player1.value}: `;
+        name2.innerText = `${player2.value}: `;
 
+        let newList = getList();
+        newList.forEach((el) => {
+            let li = document.createElement("li");
+            li.innerText = el;
+            list.appendChild(li);
+        });
+    }
 });
 
 
@@ -35,14 +68,41 @@ newGame.addEventListener("click", () => {
 // CREATE A LIST OF CATEGORIES
 
 const categories = [
-    ["Clothes", "Vehicles", "Jobs", "Things of the Universe", "Movie characters"],
-    ["Animals", "Movies", "TV series", "Something yellow", "Videogames"],
-    ["Furniture", "Books", "Natural spaces", "Verbs", "Buildings"],
-    ["Relationships", "Sports", "Something red", "In a big company", "Ecology"],
-    ["Emotions", "Weapons", "Healthy lifestyle", "Cities", "Sport equipment"],
-    ["Tools", "Climate", "Adjectives", "Insects", "Strengths"],
-    ["In the kitchen", "Recreational activities", "Agriculture", "Technology", "Toodler things"],
-    ["Plants", "Elder things", "Food", "In the city", "Countries"],
-    ["In the countryside", "Science", "Romantic things", "Trees", "Music"],
-    ["Art", "Flowers", "Subjects", "In a stationer's shop", "Geniuses (science, art, music...)"]
+    "Clothes", "Vehicles", "Jobs", "Things of the Universe", "Movie characters",
+    "Animals", "Movies", "TV series", "Something yellow", "Videogames",
+    "Furniture", "Books", "Natural spaces", "Verbs", "Buildings",
+    "Relationships", "Sports", "Something red", "In a big company", "Ecology",
+    "Emotions", "Weapons", "Healthy lifestyle", "Cities", "Sport equipment",
+    "Tools", "Climate", "Adjectives", "Insects", "Strengths",
+    "In the kitchen", "Recreational activities", "Agriculture", "Technology", "Toodler things",
+    "Plants", "Elder things", "Food", "In the city", "Countries",
+    "In the countryside", "Science", "Romantic things", "Trees", "Music",
+    "Art", "Flowers", "Subjects", "In a stationer's shop", "Geniuses (science, art, music...)"
 ]
+
+
+
+// RANDOM ITEMS FROM THE CATEGORIES LIST
+
+function random(arr) {
+    return Math.floor(Math.random() * arr.length);
+}
+
+function getList() {
+
+    let list = [];
+
+    while (list.length <= 5) {
+        let category = "";
+        category = random(categories);
+        if (!list.includes(category)) {
+            list.push(category);
+        }
+    }
+
+    let newList = [categories[list[0]], categories[list[1]], categories[list[2]], categories[list[3]], categories[list[4]]];
+
+    return newList;
+}
+
+console.log(getList());
